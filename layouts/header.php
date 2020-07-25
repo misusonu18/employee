6d3f5491f10d2632cb872b94ca51792dd8095ba9
misusonu18 <mishal@freshbits.in>
2020-07-25 19:47:43 +0530

    changes removed icons

--- layouts/header.php
+++ layouts/header.php
@@ -42,9 +42,7 @@
 			<hr class="sidebar-divider d-none d-md-block">
 
 			<div class="text-center d-none d-md-inline">
-				<button class="rounded-circle border-0" id="sidebarToggle">
-					<i class="fa fa-angle-left"></i>
-				</button>
+				<button class="rounded-circle border-0" id="sidebarToggle"></button>
 			</div>
 		</ul>
 
