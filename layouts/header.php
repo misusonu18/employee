3e24a220f5434196a527e0fbc60943859bb2b0fb
misusonu18 <misusonu18@gmail.com>
2020-08-22 22:21:59 +0530

    added font-awesome offline

--- layouts/header.php
+++ layouts/header.php
@@ -9,7 +9,8 @@
 
     <title>Employees CRUD</title>
 
-	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link href="images/logo.jpg" rel="icon" type="image/jpg" />
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/alertify.min.css">
